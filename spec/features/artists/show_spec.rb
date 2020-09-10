98e08f2be2df8e22cf8f05a5c1a100c8fb89a1b7
stellakunzang <stella.k.bonnie@gmail.com>
2020-09-10 16:38:24 -0600

    Prepare test for using factories

--- spec/features/artists/show_spec.rb
+++ spec/features/artists/show_spec.rb
@@ -20,7 +20,25 @@ RSpec.describe "artist show page" do
 
     visit "/artists/#{@artist1.id}"
 
-    expect(page).to have_content(@painting1.name)
-    expect(page).to have_content(@painting1.description)
+    expect(page).to have_css(".painting", count: 1)
+
+    within(first(".painting")) do
+      expect(page).to have_css(".name")
+      name = find(".name").text
+      expect(name).to_not be_empty
+
+      expect(page).to have_css(".description")
+      description = find(".description").text
+      expect(description).to_not be_empty
+
+      expect(page).to have_css(".medium")
+      medium = find(".medium").text
+      expect(medium).to_not be_empty
+
+      expect(page).to have_css(".color_palette")
+      color_palette = find(".color_palette").text
+      expect(color_palette).to_not be_empty
+    end
+
   end
 end
